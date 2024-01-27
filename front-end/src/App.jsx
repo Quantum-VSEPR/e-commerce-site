a33760a00c2a96014025a3a0f1034b110f93f01d
Eyob Degefu <eyobdegefu4@gmail.com>
2024-01-27 04:27:34 -0900

    the front end part is finished

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 function App() {
-  return <h1>Welcome to Proshop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
