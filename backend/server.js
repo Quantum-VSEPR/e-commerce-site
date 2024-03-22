aba76f02a193b64ded8ff0eaa11252f66e764252
Eyob Degefu <eyobdegefu4@gmail.com>
2024-03-22 11:38:08 +0300

    Implemented secure backend authentication system with robust encryption and user authentication. Committing changes for enhanced system security and user authentication.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoute from "./routes/productRouter.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
 app.use("/api/products", productRoute);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
